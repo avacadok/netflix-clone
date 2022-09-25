d4eacda15bc7ffd01aa793b4558d06a75f37b2ed
avacadok <avakuang8@gmail.com>
2022-09-25 14:46:05 -0700

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC3QzSxusbrO8mJKVJwrPV-wKrNJAOjb04",
+  authDomain: "netflix-clone-769c6.firebaseapp.com",
+  projectId: "netflix-clone-769c6",
+  storageBucket: "netflix-clone-769c6.appspot.com",
+  messagingSenderId: "55671778257",
+  appId: "1:55671778257:web:39b201f2554c3498968ef0"
+};
