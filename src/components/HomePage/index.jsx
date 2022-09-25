80a1ab09af3634bc2dfe1b4a2a84f67df4799cac
avacadok <avakuang8@gmail.com>
2022-09-25 14:51:45 -0700

    add homePage components

---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../../styles/homePage.css"
+import Navbar from './Navbar';
+
+function HomePage() {
+  return (
+    <div className='homePage'>
+      {/* Nav */}
+      <Navbar />
+      
+      {/* Banner */}
+
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default HomePage;
