6f12e6ed5f77ce5dba6aaf4c4520a10678beaa8f
avacadok <avakuang8@gmail.com>
2022-09-30 15:24:08 -0700

    add login componets

---
+++ src/components/Login/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../styles/loginPage.css'
+
+function index() {
+  return (
+    <section className='login'>
+      <div className='login-background'>
+      <img className='login-logo'
+      src='https://imgur.com/mcwxjvi.png' alt='login-background' />
+
+      <button className='login-button'>Sign In</button>
+      </div>
+      
+    </section>
+  )
+}
+
+export default index;
