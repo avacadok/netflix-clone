84b740d7b13f1f454fe19c03ed87ba7d2fd956f4
avacadok <avakuang8@gmail.com>
2022-10-04 17:57:49 -0700

    display msg on app when there is an error sign in or sign up and display welcome msg when sign up successfully

--- src/components/Login/SignIn.jsx
+++ src/components/Login/SignIn.jsx
@@ -6,27 +6,62 @@ function SignIn() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [signUp, setSignUp] = useState(false)
+  const [errMsg, setErrMsg] = useState('');
+  const [msg, setMsg] = useState('');
+  const [signUp, setSignUp] = useState(false);
 
   const handleSignIn = (e) => {
     e.preventDefault();
+    auth.signInWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        console.log('user sign in successfully', authUser.user)
+      })
+      .catch(err => {
+        console.log("auth error", err)
+        setErrMsg('Please enter a valid email and password.')
+      })
   };
 
   const handleSignUp = (e) => {
     e.preventDefault();
 
-    auth
-      .createUserWithEmailAndPassword(email, password)
+    auth.createUserWithEmailAndPassword(email, password)
       .then((authUser) => {
-        console.log('authUser', authUser)
+        console.log('authUser', authUser.user)
+        setEmail('');
+        setPassword('');
+        setErrMsg('');
+        setName('');
+        setMsg(`Welcome to Netflix ${name}. Sign in to discover more.`)
       })
-      .catch(err => console.log("auth error", err))
+      .catch(err => {
+        console.log("auth error123", err.code)
+        setMsg('')
+        //setErrMsg(err.message)
+        if (err.code == 'auth/invalid-email') {
+          setErrMsg('Please enter a valid email address')
+        }
+
+        if (err.code == 'auth/email-already-in-use') {
+          setErrMsg('This email is already in use, please enter a different email.')
+        }
+
+        if (err.code == 'auth/weak-password') {
+          setErrMsg('Password should be at least 6 characters or longer.')
+        }
+
+        if (err.code == 'auth/internal-error') {
+          setErrMsg('Password cannot be empty.')
+        }
+      })
   }
+  console.log("23131", errMsg)
 
   return (
     <div className='signIn'>
-      {!signUp ? <form onClick={handleSignIn}>
+      {!signUp ? <form >
         <h1>Sign In</h1>
+        {errMsg ? <p className='errmsg'>{errMsg}</p> : <></>}
         <input placeholder='Email'
           type={'email'}
           onChange={(e) => setEmail(e.target.value)}
@@ -37,21 +72,24 @@ function SignIn() {
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
-        <button type='Submit'>Sign In</button>
+        <button type='Submit' onClick={handleSignIn}>Sign In</button>
 
         <div>
           <span className='new-to'>New to Netflix?</span>
-          <span className='signup-link' onClick={() => { setSignUp(true) }}>Sign up now.</span>
+          <span className='signup-link' onClick={() => { setSignUp(true); setErrMsg('') }}>Sign up now.</span>
         </div>
-      </form> :
-        <form onClick={handleSignIn}>
+      </form>
+        :
+        <form >
           <h1>Sign Up</h1>
-          <input placeholder='Name'
-          type={'name'}
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-          required={true} />
-          
+          {errMsg ? <p className='errmsg'>{errMsg}</p> : <></>}
+          {msg ? <p className='signup-msg'>{msg}</p> : <></>}
+          <input placeholder='Username'
+            type={'name'}
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required={true} />
+
           <input placeholder='Email'
             type={'email'}
             onChange={(e) => setEmail(e.target.value)}
@@ -65,6 +103,11 @@ function SignIn() {
             required={true}
           />
           <button type='Submit' onClick={handleSignUp}>Sign Up</button>
+
+          <div>
+            <span className='new-to'>Have an account already?</span>
+            <span className='signup-link' onClick={() => { setSignUp(false); setErrMsg('') }}>Sign in now.</span>
+          </div>
 
         </form>
       }
