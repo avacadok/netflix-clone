77933a7e7ab882610d55aea15660fc187b5e62ac
avacadok <avakuang8@gmail.com>
2022-10-08 15:26:52 -0700

    display stripe product plan on app

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -7,26 +7,41 @@ function Plan() {
 
   useEffect(() => {
     db.collection("products").where("active", "==", true)
-    .get().then(querySnapshot => {
-      const products = {}
-      querySnapshot.forEach(async productDoc => {
-        products[productDoc.id] = productDoc.data();
-        const priceSnap = await productDoc.ref.collection('prices')
-        .get();
-        priceSnap.docs.forEach(price => {
-          products[productDoc.id].prices = {
-            priceId: price.id,
-            priceData: price.data()
-          }
+      .get().then(querySnapshot => {
+        const products = {}
+        querySnapshot.forEach(async productDoc => {
+          products[productDoc.id] = productDoc.data();
+          const priceSnap = await productDoc.ref.collection('prices')
+            .get();
+          priceSnap.docs.forEach(price => {
+            products[productDoc.id].prices = {
+              priceId: price.id,
+              priceData: price.data()
+            }
+          })
         })
+        setProducts(products)
       })
-      setProducts(products)
-    })
   }, [])
   console.log(products)
   return (
     <div className='plan'>
+      {Object.entries(products).map((product, index) => {
+        console.log("product", product[1].name)
+        console.log("productdata", index)
+        return (
+          <div className='plan-detail'>
+            <div className='plan-info'>
+              <h3>{product[1].name}</h3>
+              <p>{product[1].description}</p>
+            </div>
 
+            <button>
+              Subscribe
+            </button>
+          </div>
+        )
+      })}
     </div>
   )
 }
