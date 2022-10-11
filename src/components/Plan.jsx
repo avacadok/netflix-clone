b640bdc1d1db3fc4f498c4fdfcf26c6a020277af
avacadok <avakuang8@gmail.com>
2022-10-10 17:18:46 -0700

    style profile plan page

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -8,6 +8,25 @@ import { loadStripe } from '@stripe/stripe-js'
 function Plan() {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState('');
+
+  useEffect(() => {
+    db.collection('customers')
+      .doc(user.uid)
+      .collection('subscriptions')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async subscription => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_start: subscription.data().current_period_start.seconds,
+            current_period_end: subscription.data().current_period_end.seconds,
+          })
+        })
+      })
+  }, [user.uid])
+
+  console.log("sub", subscription)
 
   useEffect(() => {
     db.collection("products").where("active", "==", true)
@@ -48,25 +67,26 @@ function Plan() {
         const stripe = await loadStripe('pk_test_51LQ0ohBeKquJpCIYOHVFeQR22jEHa70BvOx7S7HqxVIFqHFOHl72mpQGhEL4d6doFLEoXg9rdl08ApFNy00cq6Lm00clY24t1N')
         stripe.redirectToCheckout({ sessionId })
       }
-
     })
   }
+
   return (
     <div className='plan'>
+      {subscription && <p className='renewal-date'>Renewal Date: {new Date(subscription?.current_period_end * 1000).toLocaleDateString()}</p>}
       {Object.entries(products).map(([productId, productData]) => {
-        console.log("productId", productId)
-        console.log("productdata", productData)
+        const currentPackage = productData.name.toLowerCase().includes(subscription?.role)
         return (
-          <div className='plan-detail'>
+          <div key={productId} 
+          className={` ${currentPackage && 'current-plan'} plan-detail`}>
             <div className='plan-info'>
               <h3>{productData.name}</h3>
               <p>{productData.description}</p>
             </div>
 
             <div>
-              <button onClick={() => handleCheckout(productData.prices.priceId)}
+              <button onClick={() => !currentPackage && handleCheckout(productData.prices.priceId)}
                 className="plan-button">
-                Subscribe
+                {currentPackage ? 'Current Package' : 'Subscribe'}
               </button>
             </div>
           </div>
